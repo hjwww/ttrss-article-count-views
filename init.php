4d3349f15b626221212e29b2d2bd46b9cba545dd
Hakim J. Washington <hjw@hjwashington.com>
2013-10-02 22:28:25 -0400

    Update init.php
    added upsert for

--- init.php
+++ init.php
@@ -23,6 +23,18 @@ class article_count_views extends Plugin {
 		$result = db_query("UPDATE ttrss_user_entries 
 			SET viewed = true, last_viewed = NOW()
 			WHERE ref_id = '$id' AND owner_uid = " . $_SESSION["uid"]);
+		/* placeholder for correct db table 
+		INSERT INTO `plugin_article_count_views` (
+		`user_article_id` ,
+		`viewed` ,
+		`last_viewed`
+		)
+		VALUES (
+		'$id', '1', NOW()
+		)
+		ON DUPLICATE KEY UPDATE
+		`viewed` = `viewed` + 1, last_viewed = now();
+		*/
 
 		if (db_affected_rows($result) != 0)
 			print json_encode(array("acview" => 1, "id" => $id));
